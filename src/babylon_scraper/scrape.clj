9854738720f2b0d4592586c0a07fa42c644161b3
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2016-08-23 14:50:12 +0100

    adding a few more comments

--- src/babylon_scraper/scrape.clj
+++ src/babylon_scraper/scrape.clj
@@ -16,10 +16,12 @@
 (defn generate-index-urls []
   (map #(clojure.string/join "" [base-url %]) (concat (char-range \A \Z) (char-range \0 \9))))
 
+;; This method is supposed to fetch content for given condition page URL
 (defn page-content [url]
-  
+  "TODO: fetch content"
   )
 
+;; This method fetch html content and return json-like objects
 (defn fetch-json-content [url]
   (map (fn [x] {:page-title (first (:content x))
                :page-url (:href (:attrs x))
@@ -29,12 +31,14 @@
 (defn generate-condition-urls []
   (map (fn [x] (fetch-json-content x)) (generate-index-urls)))
 
+;; this method writes the content to a file
 (defn scrape [filename]
   (with-open [wrtr (io/writer filename)]
     (doall (map (fn [x] (.write wrtr (str (json/write-str x) "\n")))
                 (flatten (generate-condition-urls))))))
 
-;; try out functions
+;; scratchpad / work area ahead
+
 (comment (char-range \0 \9))
 
 (comment (map (fn [x] [(first (:content x)) (:href (:attrs x))]) (html/select (fetch-url "http://www.nhs.uk/Conditions/Pages/BodyMap.aspx?Index=A") [:div.index-section :> :ul :li :a])))
